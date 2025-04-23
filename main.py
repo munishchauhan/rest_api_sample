70afbd1969bb989a373687c2b46e861ada3c15c7
munish <chauhan.munish1@gmail.com>
2025-04-23 11:29:07 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,67 @@
+# print("Hello World")
+
+from flask import Flask, jsonify, make_response, send_from_directory
+from flask_swagger_ui import get_swaggerui_blueprint
+from routes import request_api
+import pypyodbc as odbc #pip install pypyodbc
+
+DRIVER_NAME = 'SQL SERVER'
+SERVER_NAME = 'HP-Elitebook'
+DATABASE_NAME = 'DBApp'
+
+connection_string = f"""
+    DRIVER={{{DRIVER_NAME}}};
+    SERVER={SERVER_NAME};
+    DATABASE={DATABASE_NAME};
+    Trust_Connection=yes;
+"""
+
+
+app = Flask(__name__)
+
+
+@app.route("/static/<path:path>")
+def send_static(path):
+    return send_from_directory('static', path)
+
+
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'Python API Sample'
+    }
+)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
+
+app.register_blueprint(request_api.get_blueprint())
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route('/api/users/<string:name>')
+def get_users(name):
+    conn = odbc.connect(connection_string)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM tbCountryList")
+    list = cursor.fetchall()   
+
+    conn.close()
+
+    return jsonify(list)
+
+    # return jsonify({
+    #     "name": f"Hello {name}"
+    # })
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
